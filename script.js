f3b7e3247c3cc2bc3c3c0e2df035038ffae10193
paula <psepulveda26@gmail.com>
2016-10-16 22:41:42 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,67 @@
+//Ejercicio N°1 "EL menor de los números"
+function numeroMenor(a,b,c){
+	var menor=Math.min(a,b,c);
+	return menor;
+}
+//Ejercicio N°2"Es par o impar"
+function numeroPar(num){
+	if (num%2 ===0){
+		alert ("es par");
+	}else{
+		alert ("es impar");
+	}
+}
+//Ejercicio N°3 "Ordenando números de menor a mayor"
+
+
+//Ejercicio N°4 "Mayúscula o minuscula"
+function mayuscula(cadena){
+	var resultado="la palabra "+cadena+" ";
+	if (cadena===cadena.toLowerCase()){
+		resultado+="son solo minúsculas";
+	}else{
+		resultado +="es ambas";
+	}
+	return resultado;
+}
+//Ejercicio N°6 "Operaciones matématicas"
+function suma (a,b){
+	return a+b;
+}
+
+function resta (a,b){
+	return a-b;
+}
+
+function multiplicar (a,b){
+	return a*b;
+}
+
+function dividir (a,b){
+	return a/b;
+}
+
+function operar (a,b, operacion){
+	return operacion(a,b);
+}
+//Ejercicio N°7"Cuadrado de la Suma de cuadrados"
+function sumaCuadrados(a,b){
+	var resultado=(a+b);
+	var cuadrado=resultado*resultado;
+	return cuadrado;
+}
+//ejercicio N°8 "Número Primo"
+function numeroPrimo(num){
+	var i= 2;
+	while (i*i<num){
+	if(num%i==0)
+	return (false);
+	i++;
+	}
+	return (true);
+}
+
+//Ejercicio N°9 "Multiplos de un número"
+
+	
+
